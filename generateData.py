8028c3138ded57d821b1b9decaa7a4633a509d93
quechulo <aawangardowy@gmail.com>
2022-05-11 16:56:43 +0200

    script for generating data creaated!

---
+++ generateData.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+np.random.seed(305862)  # lowest index
+
+
+def generatePopulation(N):
+    pop = []
+    pop_weights = np.random.uniform(0.1, 1, (N, 1))
+    pop_p = np.random.uniform(1, 100, (N, 1))
+
+    for i in range(0, N):
+        pop_weights[i][0] = np.round(pop_weights[i][0], 1)
+        pop_p[i][0] = np.round(pop_p[i][0])
+        chap = [pop_weights[i][0], pop_p[i][0]]
+        pop.append(chap)
+    print(pop)
+    return pop
+
+if __name__ == "__main__":
+    generatePopulation(32)
