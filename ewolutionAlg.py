5aed6e13c20afee8584a2b7b09cb08d86b123cb6
quechulo <aawangardowy@gmail.com>
2022-05-11 18:36:58 +0200

    added real generatingPopulation script and methods for calculating Weight and Value of knapsack

--- ewolutionAlg.py
+++ ewolutionAlg.py
@@ -1,12 +1,14 @@
-from generateData import generatePopulation
+from generateData import generateItems, generatePopulation
 import numpy as np
 
 N = 32
 
 
-if __name__ == "__main__":
-    pop = generatePopulation(N)
-    W = np.round(np.sum(pop, axis=0)[0] * 0.3, 1)  # max weight of knapsack
 
+if __name__ == "__main__":
+    pop = generateItems(N)
+    print(pop)
+    W = np.round(np.sum(pop, axis=0)[0] * 0.3, 1)  # max weight of knapsack
+    print(W)
 
 
--- generateData.py
+++ generateData.py
@@ -1,21 +1,49 @@
 import numpy as np
 
-INDEX = 305862
+INDEX = 305858
 np.random.seed(INDEX)  # lowest index
 
 
-def generatePopulation(N):
-    pop = []
+def generateItems(N):
+    items = []
     pop_weights = np.random.uniform(0.1, 1, (N, 1))
     pop_p = np.random.uniform(1, 100, (N, 1))
 
     for i in range(0, N):
         pop_weights[i][0] = np.round(pop_weights[i][0], 1)
         pop_p[i][0] = np.round(pop_p[i][0])
-        chap = [pop_weights[i][0], pop_p[i][0]]
-        pop.append(chap)
-    # print(pop)
-    return pop
+        item = [pop_weights[i][0], pop_p[i][0]]
+        items.append(item)
+    # print(items)
+    return items
+
+def calcWeight(rep, items):
+    weight = 0
+    for i in range(0, len(items)):
+        weight += rep[i] * items[i][0]
+    return weight
+
+def calcValue(rep, items):
+    value = 0
+    for i in range(0, len(items)):
+        value += rep[i] * items[i][1]
+    return value
+
+def generatePopulation(items, pop_size, W_max):
+    population = []
+    for i in range(0, pop_size):
+        rep = np.zeros(32, dtype=int)
+        while(calcWeight(rep, items) < 0.7 * W_max):  # first representants not the best, only around 70% good
+            rep[np.random.randint(len(items))] = 1
+        population.append(rep)
+        print(calcValue(rep, items))
+    return population
+
+
 
 if __name__ == "__main__":
-    generatePopulation(32)
+
+    items = generateItems(32)
+    print(items)
+    population = generatePopulation(items, 5, 5.8)
+    print(population)
